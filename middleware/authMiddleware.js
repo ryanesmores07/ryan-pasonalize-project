93143dadcf4c25d716fcf802e328501e714a0f7b
Ryan Esmores <eesmores@fab.pasona.tech>
2024-01-31 17:21:34 +0900

    Continue with authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+  BadRequestError,
+} from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
+
+export const authenticateUser = (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("authentication invalid");
+  try {
+    const { userId, role } = verifyJWT(token);
+    req.user(userId, role);
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 // routers
 import userRouter from "./routes/userRouter.js";
@@ -13,6 +14,7 @@ import authRouter from "./routes/authRouter.js";
 
 // middlewares
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 const app = express();
 const port = process.env.PORT || 5100;
@@ -21,6 +23,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -28,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({
