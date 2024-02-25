b0e36bc53804372a4ae19be392e1fa702882e9cb
Ryan Esmores <ryanesmores07@gmail.com>
2024-02-26 00:12:01 +0900

    Changed the background image. Continue with the logos below the texts

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
-import heroImage from "../assets/images/LandingPage/main-image.png";
+import heroImage from "../assets/images/LandingPage/bg-hero.png";
 
 const Landing = () => {
   const navigate = useNavigate();
@@ -9,7 +9,11 @@ const Landing = () => {
       <div className="container">
         <div className="left-section">
           <div className="text-container">
-            <h2>LET YOUR PEERS GET TO KNOW YOU MORE WITH</h2>
+            <h2>
+              LET YOUR <span style={{ color: "#35d67e" }}>PEERS</span> GET TO
+              <span style={{ color: "#b092f0" }}> KNOW </span>YOU{" "}
+              <span style={{ color: "#ffbf00" }}>MORE</span> WITH
+            </h2>
             <h1>PASONALIZE</h1>
             <div className="divider"></div>
             <p>
@@ -35,9 +39,7 @@ const Landing = () => {
           </div>
         </div>
         <div className="right-section">
-          <div className="image-container">
-            <img src={heroImage} alt="hero-image" className="hero-image" />
-          </div>
+          <div className="image-container"></div>
         </div>
       </div>
     </Wrapper>
@@ -45,19 +47,22 @@ const Landing = () => {
 };
 
 const Wrapper = styled.section`
-  max-width: 1440px;
   .container {
-    margin: 161px 120px;
+    height: 100vh;
     display: flex;
-    gap: 42px;
     justify-content: center;
-    /* background-color: lightyellow; */
+    align-items: center;
+    background-image: url(${heroImage});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 
     .left-section {
       width: 50%;
+      margin-left: 120px;
       .text-container {
         h2 {
-          max-width: 45ch;
+          max-width: 70%;
           font-size: 4.5rem;
           font-weight: var(--font-weight-black);
         }
@@ -86,22 +91,32 @@ const Wrapper = styled.section`
         margin-top: 5.5rem;
         .register-btn,
         .login-btn {
+          font-size: 1.6rem;
           width: 183px;
           height: 5.6rem;
           border-radius: 50rem;
+          border-style: none;
+          font-weight: 600;
+          font-family: "Montserrat";
+          cursor: pointer;
         }
         .register-btn {
           color: #ffffff;
           background-color: var(--blue);
-          font-weight: 600;
+        }
+        .login-btn {
+          border-color: var(--blue);
+          border: 1.5px solid var(--blue);
+          color: var(--blue);
+          background-color: rgba(54, 169, 236, 0.05);
         }
       }
     }
     .right-section {
+      display: flex;
+      justify-content: flex-end;
       width: 50%;
       .image-container {
-        max-width: 595px;
-        height: 639px;
       }
     }
   }
